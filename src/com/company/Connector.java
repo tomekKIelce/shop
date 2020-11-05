9f1c8478f6e70d8fb2c715b96d0130128167a9dd
tomekKIelce <tomaszszybicki@gmail.com>
2020-11-05 22:13:04 +0100

    add connector

---
+++ src/com/company/Connector.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.*;
+
+public class Connector {
+
+    public Connection con;
+
+        public void connetToDb(){
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop", "root", "");
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+}
