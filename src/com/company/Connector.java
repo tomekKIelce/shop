07aaa1e72fc1c72bd156c576dab91dfa6fe7eb21
tomekKIelce <tomaszszybicki@gmail.com>
2020-11-06 19:45:03 +0100

    add  select querry in ShowAllProducts

--- src/com/company/Connector.java
+++ src/com/company/Connector.java
@@ -13,19 +13,6 @@ public class Connector {
                 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop", "root", "");
                 PreparedStatement prst = con.prepareStatement("SELECT * FROM products");
                 ResultSet rs = prst.executeQuery();
-                while (rs.next()){
-                    System.out.println("produkty :"
-                            +rs.getInt("idProduct"+1)
-                            +rs.getString("nazwa produktu:"+2)
-                            +rs.getDate("data sporzycia"+3)
-                            +rs.getInt("cena produktu:" +4)
-                            +rs.getInt("ilosc sztuk w sklepie"+5)
-                            +rs.getInt("kod kreskowy"+6)
-                            +rs.getInt("waga produktu"+7)
-                            +rs.getInt("kod produktu"+8)
-
-                    );
-                }
 
 
             } catch (ClassNotFoundException | SQLException e) {
--- src/com/company/ShowAllProducts.java
+++ src/com/company/ShowAllProducts.java
@@ -9,9 +9,27 @@ public class ShowAllProducts {
 
     Connector conn = new Connector();
     public void showAllProducts(){
-        conn.connetToDb();
 
+        try {
 
+           PreparedStatement prst = conn.con.prepareStatement("SELECT * FROM products");
+            ResultSet rs = prst.executeQuery();
+            while (rs.next()){
+                System.out.println(
+                        "id produktu :" +rs.getInt(1) +
+                                " nazwa produktu :"  +rs.getString(2)+
+                                " data sporzycia :" +rs.getDate(3)+
+                                " cena:"  +rs.getInt(4)+
+                                " ilość sztuk na sklepie :"  +rs.getInt(5)+
+                                " kod kreskowy :"  +rs.getInt(6)+
+                                " waga  :"  +rs.getInt(7)+
+                                " kod produktu  :"  +rs.getInt(8)
+
+                );
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
 
     }
